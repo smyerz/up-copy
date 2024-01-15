6758532b81a73d2e382b34f641f02694b5fbfaab
Maugrins <shadowboxartsstore@gmail.com>
2024-01-15 11:26:07 -0800

    clean app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+//import Heading from './Heading';
+import React, { useState } from 'react';
+//import ReactPlayer from 'react-player/youtube'; //will accept youtube urls
+//import component from "./name";
+import {Routes, Route} from 'react-router-dom';
+
+//const or function? using function and cost for var makes it easier to differentiate between var and function
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App Name">
+
+     {/*<header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,7 +23,24 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+
+  </header>*/}
+
+      <nav className='nav'>
+        <a href='#' className='nav-item'>page name</a>
+      </nav>
+
+      {/*<Routes>
+        <Route path='/'element={<page name />}/>
+        </Routes>*/}
+
+      {
+      /*<video/>
+      const Video =()+return (<ReactPlayer url="video url"/>);*/}
+     
+    {/*const [word.setWord]=React.useState("")*/}
+      
+
     </div>
   );
 }
